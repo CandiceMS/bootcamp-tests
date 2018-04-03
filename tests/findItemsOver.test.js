a72f99a8b7727de71d115989a4d1bfd8d8050ae5
Candice <candice.m.sprout@gmail.com>
2018-04-03 16:58:40 +0200

    Andre Changes

--- tests/findItemsOver.test.js
+++ tests/findItemsOver.test.js
@@ -14,8 +14,9 @@ describe('Quantity greater than the threshold', function(){
     {name : 'apples', qty : 10},
     {name : 'pears', qty : 27},
     {name : 'bananas', qty : 37},
-    {name : 'apples', qty : 3}], 25),
-    [{name : 'pears', qty : 27},
+    {name : 'apples', qty : 3}], 9),
+    [{name : 'apples', qty : 10},
+    {name : 'pears', qty : 27},
     {name : 'bananas', qty : 37}]);
     });
 });
--- tests/findItemsOver20.test.js
+++ tests/findItemsOver20.test.js
@@ -12,10 +12,12 @@ describe('Quantity over 20', function(){
     it('should return all items with a quantity greater than 20', function(){
         assert.deepEqual( findItemsOver20([
     {name : 'apples', qty : 10},
-    {name : 'pears', qty : 27},
-    {name : 'bananas', qty : 37},
-    {name : 'apples', qty : 3}]),
-    [{name : 'pears', qty : 27},
-    {name : 'bananas', qty : 37}]);
+    {name : 'pears', qty : 7},
+    {name : 'bananas', qty : 3},
+    {name : 'apples', qty : 13}]),
+    []);
+    });
+    it('should return all items with a quantity greater than 20', function(){
+        assert.deepEqual( findItemsOver20([]),[]);
     });
 });
